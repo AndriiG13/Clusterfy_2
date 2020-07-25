a245952691da1ab6554d77647eaf231a9f95a718
Andrii Grygoryshyn <47453561+AndriiG13@users.noreply.github.com>
2020-07-25 14:57:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ secret_user = randomword(100)
 app.config['SECRET_KEY'] = secret_user
 
 ####################################################################################################
-client_id = "d5ec308d7b9749b5b49c3e4da6ef4da2"
-client_secret = "0e137083f0b149e8afa2eaa57f59fb94"
+client_id = "XXXXXXXXXXX"
+client_secret = "XXXXXXXXXXXXXXXXXX"
 
 spotify_authentification_url = "https://accounts.spotify.com/authorize"
 spotify_token_url = "https://accounts.spotify.com/api/token"
